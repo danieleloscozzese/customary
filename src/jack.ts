c2327157953170f70abd164ca439ed6be09eb554
Daniel Gallagher <daniel.gallagher@bbc.co.uk>
2017-11-10 12:30:49 +0000

    New dependencies
    I have added Prettier for formatting and updated my dependencies
    
    I have added an npm 5 lockfile.

--- src/jack.ts
+++ src/jack.ts
@@ -1,14 +1,20 @@
-import { relyOn } from 'rely-on';
+import { relyOn } from "rely-on";
 
 /**
  * Sets up a form with an event listener on the submit event that prevents
  * submission.
  */
-export default function preventFormSubmission(formId: string, callback?: (form: HTMLFormElement) => void): void {
-  const form = relyOn(document.getElementById(formId), `There is no element with ID ${formId} in the document.`);
+export default function preventFormSubmission(
+  formId: string,
+  callback?: (form: HTMLFormElement) => void
+): void {
+  const form = relyOn(
+    document.getElementById(formId),
+    `There is no element with ID ${formId} in the document.`
+  );
 
   if (form instanceof HTMLFormElement) {
-    const submitHandler = function (submission: Event) {
+    const submitHandler = function(submission: Event) {
       submission.preventDefault();
 
       if (callback) {
@@ -16,9 +22,13 @@ export default function preventFormSubmission(formId: string, callback?: (form: 
       } else {
         return;
       }
-    }
+    };
 
-    form.addEventListener('submit', submitHandler, <any>{ capture: true, passive: false, once: false });
+    form.addEventListener("submit", submitHandler, <any>{
+      capture: true,
+      passive: false,
+      once: false
+    });
   } else {
     throw new TypeError(`The element with id ${formId} is not a form.`);
   }
